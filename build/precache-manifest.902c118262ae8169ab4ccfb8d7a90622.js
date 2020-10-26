06c9b6f8886db77595399ea1cefdb763016feee2
ShaharEli <shahar.e3@gmail.com>
2020-10-26 14:54:05 +0200

    make it better

---
+++ build/precache-manifest.902c118262ae8169ab4ccfb8d7a90622.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d77615ebce680cc50ce2afcbe0b56256",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaf30ef957715671cdfb",
+    "url": "/static/css/main.9302eb05.chunk.css"
+  },
+  {
+    "revision": "4eb589ea591bdbf37707",
+    "url": "/static/js/2.09b93fe3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.09b93fe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaf30ef957715671cdfb",
+    "url": "/static/js/main.4a2c456f.chunk.js"
+  },
+  {
+    "revision": "a86ae9ebc2c7d5259355",
+    "url": "/static/js/runtime-main.2a8d9987.js"
+  }
+]);
